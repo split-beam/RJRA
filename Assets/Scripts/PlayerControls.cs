fec187245dc007199dbd1c4c22424646f252dd3a
splitbeam <qvdeutekom@icloud.com>
2021-08-31 13:35:10 +0200

    Signed-off-by: splitbeam <qvdeutekom@icloud.com>

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 7f;
 
     // Start is called before the first frame update
     void Start()
@@ -14,18 +16,30 @@ public class PlayerControls : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    void ProcessTranslation()
+    {
         float xthrow = Input.GetAxis("Horizontal");
         float ythrow = Input.GetAxis("Vertical");
 
+
         float xOffset = xthrow * Time.deltaTime * controlSpeed;
-        float newXPos = transform.localPosition.x + xOffset;
+        float RawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(RawXPos, -xRange, xRange);
 
         float yOffset = ythrow * Time.deltaTime * controlSpeed;
-        float newYPos = transform.localPosition.y + yOffset;
+        float RawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(RawYPos, -yRange, yRange);
 
-        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
-        //Debug.Log(xthrow);
-        //Debug.Log(ythrow);
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    void ProcessRotation()
+    {
+        transform.localRotation = Quaternion.Euler(-30f, 30f, 0f);
     }
 }
