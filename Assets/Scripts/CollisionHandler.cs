6e3f81e3e6072a376f4cf9572a638366b91d06ab
splitbeam <qvdeutekom@icloud.com>
2021-09-08 14:17:25 +0200

    Signed-off-by: splitbeam <qvdeutekom@icloud.com>

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log($"{this.name} **Collided With** {collision.gameObject.name}");
+        //Debug.Log(this.name + "--Collided With--" + collision.gameObject.name);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
