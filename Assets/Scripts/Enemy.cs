f9fa6d725db9215100c9d2ebe7259a34c0a6f62b
splitbeam <qvdeutekom@icloud.com>
2021-09-09 11:51:50 +0200

    Signed-off-by: splitbeam <qvdeutekom@icloud.com>

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scoreIncerase = 15;
+
+    ScoreBoad scoreBoad;
+
+    void Start()
+    {
+        scoreBoad = FindObjectOfType<ScoreBoad>();
+    }
+
+    bool exploding = false;
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log($" {name} Is Hit By {other.gameObject.name}");
+        if (exploding) { return; }
+        ProcessHit();
+        KillEnemy();
+    }
+
+    void ProcessHit()
+    {
+        scoreBoad.IncreaseScore(scoreIncerase);
+    }
+    void KillEnemy()
+    {
+        exploding = true;
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
+
+   
 }
