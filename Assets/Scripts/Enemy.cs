d86c94bfa20d25517e855a9aea85ad9df8566240
splitbeam <qvdeutekom@icloud.com>
2021-09-10 13:04:21 +0200

    Audio Bits

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,33 +5,51 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] int scoreIncerase = 15;
+    [SerializeField] int hitPoints = 2;
+
+    bool exploding = false;
 
     ScoreBoad scoreBoad;
+    GameObject parentGameObject;
 
     void Start()
     {
         scoreBoad = FindObjectOfType<ScoreBoad>();
+        AddRigidBody();
     }
 
-    bool exploding = false;
+    void AddRigidBody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRunTime");
+        rb.useGravity = false;
+    }
+
     void OnParticleCollision(GameObject other)
     {
         if (exploding) { return; }
         ProcessHit();
-        KillEnemy();
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
     }
 
     void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
+        hitPoints--;
         scoreBoad.IncreaseScore(scoreIncerase);
     }
     void KillEnemy()
     {
         exploding = true;
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
+        scoreBoad.IncreaseScore(scoreIncerase*2);
         Destroy(gameObject);
     }
 
